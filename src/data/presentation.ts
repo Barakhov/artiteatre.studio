4497bf942bef2c7cb2cdc9d3d105c3f11144f8e5
Alex Barakhov <alex.barakhov@tui.com>
2025-02-11 11:41:14 +0100

    wip

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -11,22 +11,18 @@ type Presentation = {
 };
 
 const presentation: Presentation = {
-  mail: "a.barakhov@gmail.com",
-  title: "Hi there, I'm Alex 👋",
+  mail: "artiteatre.nadiabanegas@gmail.com",
+  title: "Nadia Banegas - artiteatre.studio",
   description:
-    "I'm a frontend developer with a robust background in visual design, specializing in user interface development. I build intuitive, engaging interactions using technologies like Svelte, React, Tailwind, 11ty, Webflow, GSAP, and Rive. Passionate about web animations, I prioritize user experience, accessibility, and best practices. Skilled in cross-functional collaboration, I simplify complex problems and stay ahead of industry trends.",
+    "Nam a purus fermentum, viverra turpis ac, convallis est. Morbi congue nibh vel lacus pretium euismod. Nunc lectus est, dapibus at iaculis ac, accumsan at justo. Integer gravida interdum posuere.",
   socials: [
     {
-      label: "LinkedIn",
-      link: "https://www.linkedin.com/in/alex-barakhov-103b26a9/",
+      label: "Facebook",
+      link: "https://www.facebook.com/Musicaldansa/",
     },
     {
-      label: "X (Twiiter)",
-      link: "https://x.com/a_barakhov",
-    },
-    {
-      label: "Github",
-      link: "https://github.com/Barakhov",
+      label: "Instagram",
+      link: "https://www.instagram.com/musicaldansa/",
     },
   ],
 };
