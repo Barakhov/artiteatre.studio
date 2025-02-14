d99bc2c58a5c5708aea8e341b76cbe8c4208afc3
Alex Barakhov <alex.barakhov@tui.com>
2025-02-14 12:35:53 +0100

    shapes

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -12,7 +12,7 @@ type Presentation = {
 
 const presentation: Presentation = {
   mail: "artiteatre.nadiabanegas@gmail.com",
-  title: "Nadia Banegas - artiteatre.studio",
+  title: "artiteatre",
   description:
     "ARTITEATRE es una escuela en la que se imparten diferentes materias que tienen que ver con los intereses de su fundadora Nadia Banegas. Generando así tres líneas muy diferenciadas con una estructura y planteamiento propio en cada una de ellas.",
   socials: [
