667460940ad49a59cab607d8a5cfa9b050b39040
Alex Barakhov <alex.barakhov@tui.com>
2025-02-15 08:54:12 +0100

    gbzeg   q

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -7,6 +7,9 @@ type Presentation = {
   mail: string;
   title: string;
   description: string;
+  phone: string;
+  address: string;
+  gmaps: string;
   socials: Social[];
 };
 
@@ -14,7 +17,7 @@ const presentation: Presentation = {
   mail: "artiteatre.nadiabanegas@gmail.com",
   title: "artiteatre",
   description:
-    "Una escuela en la que se imparten diversas materias inspiradas en los intereses de su fundadora, Nadia Banegas, dando origen a tres líneas diferenciadas, cada una con su propia estructura y enfoque.",
+    "Una escuela que imparte diversas materias inspiradas en los intereses de su fundadora, Nadia Banegas, dando origen a tres iniciativas educativas, cada una con su propia estructura y enfoque.",
   socials: [
     {
       label: "Facebook",
@@ -25,6 +28,9 @@ const presentation: Presentation = {
       link: "https://www.instagram.com/musicaldansa/",
     },
   ],
+  phone: "971 31 31 47 / 666 231 065",
+  address: "Pasaje Isidoro Macabich Edfo Olimpo, 20 bis, 6º 3ª 07800 Eivissa",
+  gmaps: "https://maps.app.goo.gl/RMRaKpxjRpi3fPYS8"
 };
 
 export default presentation;
