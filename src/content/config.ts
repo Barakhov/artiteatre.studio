3cbc5dc76f41ecec147977e96c00edbdcda57e3b
Alex Barakhov <abarakhov@gmail.com>
2026-04-30 16:39:22 +0200

    added zod to config

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,4 +11,31 @@ const postsCollection = defineCollection({
   }),
 });
 
-export const collections = { posts: postsCollection };
+const image = z.object({ image: z.string(), caption: z.string() });
+const document = z.object({ name: z.string(), doc: z.string() });
+const teacher = z.object({
+  name: z.string(),
+  image: z.string(),
+  image_alt: z.string(),
+  description: z.string(),
+});
+const materia = z.object({
+  title: z.string(),
+  text: z.string(),
+  image: z.string(),
+  file: z.string(),
+});
+
+const veranoCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    intro: z.string().optional(),
+    images: z.array(image).optional(),
+    teachers: z.array(teacher).optional(),
+    documents: z.array(document).optional(),
+    materias: z.array(materia).optional(),
+  }),
+});
+
+export const collections = { posts: postsCollection, verano: veranoCollection };
